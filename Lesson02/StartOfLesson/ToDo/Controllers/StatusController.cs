e92c90390136a4aa50943841cb3ff6f7aed8f16e
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-10-14 15:55:47 -0500

    I am almost positive that I finished the part one prework for lesson 2, but I havent figured out getting my create and edit pages to actually function yet

---
+++ Lesson02/StartOfLesson/ToDo/Controllers/StatusController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    public class StatusController : Controller
+    {
+        private static List<Status> StatusList = new List<Status>()
+        {
+            new Status {Id = 1, Value = "Not Started" },
+            new Status {Id = 2, Value = "In Progress"},
+            new Status {Id = 3, Value = "Done" },
+            new Status {Id = 4, Value = "In Review"},
+        };
+        public IActionResult Index()
+        {
+            return View(StatusList);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit(Status Collection)
+        {
+            return View();
+        }
+    }
+}
