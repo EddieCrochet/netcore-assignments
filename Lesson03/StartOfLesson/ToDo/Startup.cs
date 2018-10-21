bb83988e59bad9cee7d067fad8b3da670db70d3a
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-10-21 16:15:08 -0500

    trying this again

--- Lesson03/StartOfLesson/ToDo/Startup.cs
+++ Lesson03/StartOfLesson/ToDo/Startup.cs
@@ -48,10 +48,11 @@ namespace ToDoApp
 
             app.Use(async (HttpContext context, Func<Task> next) =>
             {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                await next();
 
             });
 
-            //app.UseMiddleware<LoggingMiddleware>();
 
             app.UseStaticFiles();
             app.UseCookiePolicy();
@@ -64,73 +65,4 @@ namespace ToDoApp
             });
         }
     }
-    /*public class LoggingMiddleware
-    {
-        private readonly RequestDelegate _next;
-        public LoggingMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-        public async Task Invoke(HttpContext context)
-        {
-            var request = await FormatRequest(context.Request);
-            //gets the incoming request
-
-            var OriginalBodyStream = context.Response.Body;
-            //copies a pointer to the original body stream
-
-            //now create a new memory stream...
-            using (var responseBody = new MemoryStream())
-            {
-                context.Response.Body = responseBody;
-                //and use that stream for the temporary response body
-
-                await _next(context);
-                //continue through pipeline and return to this class
-
-                var response = await FormatResponse(context.Response);
-                //formats the response from the server
-
-                await responseBody.CopyToAsync(OriginalBodyStream);
-                //copy contents of new memory stream to original
-            }
-        }
-
-        private async Task<string> FormatResponse(HttpResponse response)
-        {
-            response.Body.Seek(0, SeekOrigin.Begin);
-            //read response stream from the beginning
-
-            string text = await new StreamReader(response.Body).ReadToEndAsync();
-            //copy that into a string
-
-            response.Body.Seek(0, SeekOrigin.Begin);
-            //reset reader for the response so the client can read it
-
-            return $"{response.StatusCode}: {text}";
-            //return the string for the response
-        }
-
-        private async Task<string> FormatRequest(HttpRequest request)
-        {
-            var body = request.Body;
-
-            request.EnableRewind();
-            //function allows us to set reader back at beginning of stream
-
-            var buffer = new byte[Convert.ToInt32(request.ContentLength)];
-            //we need to read request stream. Create a byte w same length as original
-
-            await request.Body.ReadAsync(buffer, 0, buffer.Length);
-            //copy the request stream into the new buffer
-
-            var bodyAsText = Encoding.UTF8.GetString(buffer);
-            //convert the byte[] into a string
-
-            request.Body = body;
-            //assign read body back to request body - now allowed because of the EnableRewind();
-
-            return $"{request.Scheme} {request.Host}{request.Path} {request.QueryString} {bodyAsText}";
-        }
-    }*/
 }
