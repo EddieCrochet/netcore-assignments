d70f1edb656f30fbfb81f58f9ad650a37a199e4e
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-10-22 01:44:49 -0500

    set up hw

---
+++ Lesson02/Lesson02/StartOfLesson/ToDo/ViewCOmponents/CardGroupViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+using ToDoApp.Services;
+
+namespace ToDoApp.ViewCOmponents
+{
+    public class CardGroupViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var oldestToDos = Repository.ToDos
+                .OrderBy(x => x.Created)
+                .Take(4)
+                .Select(todo => new CardViewModel
+                {
+                    Title = todo.Title,
+                    Summary = todo.Description
+                });
+            return View(oldestToDos);
+        }
+    }
+}
