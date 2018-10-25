8ba4076e22f190cb557140341f37cb9c6840cc22
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-10-25 17:51:48 -0500

    I believe I have finished my Lesson02 homework Part 1!

---
+++ Lesson02/Lesson02/EndOfLesson/TagHelpers/AutocompleteTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+
+namespace TagHelpers
+{
+    [HtmlTargetElement("input")]
+    //the HTML elements I want this tag helper to apply to
+
+    class AutocompleteTagHelper : TagHelper
+    {
+        [HtmlAttributeName("my-autocomplete")]
+        public string MyAutoComplete { get; set; }
+
+        /*[HtmlAttributeName("autocomplete")]
+        //what attributes I read data out of
+
+        public ModelExpression Auto{ get; set; }*/
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (MyAutoComplete == "false" || MyAutoComplete == "disabled")
+                output.Attributes.SetAttribute("autocomplete", "off");
+            else
+            {
+                output.Attributes.SetAttribute("autocomplete", "on");
+            }
+        }
+    }
+}
