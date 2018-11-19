52481300c58879793f887a7df8771c46b1072d7b
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-11-18 20:50:21 -0600

    this will be my own personal project for the future

---
+++ FinalProject/OrderingPizza/OrderingPizza/Services/Repository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OrderingPizza.Models;
+
+namespace OrderingPizza.Services
+{
+    public class Repository : IRepository
+    {
+        private List<Customer> _customers = new List<Customer>();
+
+        public Repository()
+        {
+            //fake temp data
+            _customers.Add(new Customer() { Id=1, Name="John Doe" });
+        }
+
+        public List<Customer> Customers { get { return _customers; } }
+
+        public void Add(Customer customer)
+        {
+            Customers.Add(customer);
+        }
+
+        //public Customer GetCustomer(Customer customer)
+        //{
+        //    return Customers.Remove
+        //}
+    }
+}
