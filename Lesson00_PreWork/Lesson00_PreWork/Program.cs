cc3b430273a608f63b611a1f0d05e932867901b0
Eddie Crochet <eddiecrochet1994@gmail.com>
2018-11-24 18:30:31 -0600

    All finished with this console app

--- Lesson00_PreWork/Lesson00_PreWork/Program.cs
+++ Lesson00_PreWork/Lesson00_PreWork/Program.cs
@@ -6,7 +6,42 @@ namespace Lesson00_PreWork
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            View.DisplayMethod();
+        }
+    }
+
+    public class Controller
+    {
+        public Controller()
+        {
+        }
+
+        public static void LoopNum(int RepeatingChar)
+        {
+            int RepeatNum = 10;
+            for (int i = 0; i < RepeatNum; i++)
+            {
+                Console.Write(Model.RepeatingChar);
+            }
+        }
+    }
+
+    public class Model
+    {
+        public static int RepeatingChar { get; set; }
+
+        public Model (int repeatingChar)
+        {
+            RepeatingChar = repeatingChar;
+        }
+    }
+
+    class View
+    {
+        public static void DisplayMethod()
+        {
+            Model myModel = new Model(8);
+            Controller.LoopNum(Model.RepeatingChar);
         }
     }
 }
